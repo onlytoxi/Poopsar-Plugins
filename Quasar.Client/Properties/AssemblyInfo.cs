478e8554a1b55d2de7291116bdc73f14c65c8d55
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2025-02-26 18:57:27 -0600

    Fix small issues with changing assembly name in stuff like task manager.

--- Quasar.Client/Properties/AssemblyInfo.cs
+++ Quasar.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("Quasar Modded Client")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Quasar Modded")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
