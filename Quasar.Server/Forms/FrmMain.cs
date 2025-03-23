bf5008d285a5fa6f1d3ecc500b1d92bdb5e531df
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2025-03-22 21:40:05 -0500

    remove for now because of small UI bugs.

--- Quasar.Server/Forms/FrmMain.cs
+++ Quasar.Server/Forms/FrmMain.cs
@@ -768,8 +768,8 @@ namespace Quasar.Server.Forms
                         AddStarButton(lvi, client);
                         SortClientsByFavoriteStatus();
                     }
-                    lvi.UseItemStyleForSubItems = false;
-                    lvi.SubItems[4].ForeColor = Color.Green; // Set "Connected" text color to green
+                    //lvi.UseItemStyleForSubItems = false;
+                    //lvi.SubItems[4].ForeColor = Color.Green;
                 });
                 EventLog(client.Value.UserAtPc + " Has connected.", "normal");
                 UpdateWindowTitle();
