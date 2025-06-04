5483601706cb98ddc0c69424eb2fa09e516f2902
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2025-06-04 00:45:02 -0500

    somehow fix costura bug? Not sure how the hell this is caused. Due to this code.
    [assembly: AssemblyVersion("1.6.3")]
    
    I love my life.

--- Pulsar.Common.Tests/Properties/AssemblyInfo.cs
+++ Pulsar.Common.Tests/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("cfda6d2e-8ab3-4349-b89a-33e1f0dab32b")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.3")]
-[assembly: AssemblyFileVersion("1.6.3")]
+[assembly: AssemblyVersion("1.6.4")]
+[assembly: AssemblyFileVersion("1.6.4")]
