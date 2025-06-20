6ea64a2bc520d1637b7fe3d03e5982fe81e828e7
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2025-06-19 22:16:20 -0500

    clean up repo and update some packages.

--- Pulsar.Client/Messages/KematianHandler.cs
+++ Pulsar.Client/Messages/KematianHandler.cs
@@ -21,7 +21,7 @@ namespace Pulsar.Client.Messages
 
         private void Execute(ISender client, GetKematian message)
         {
-            int defaultTimeout = 120; //after 2 minutes all hope is lost :sob:
+            //int defaultTimeout = 120; //after 2 minutes all hope is lost :sob:
             //byte[] zipFile = Handler.GetData(defaultTimeout);
             //client.Send(new GetKematian { ZipFile = zipFile });
             // this method is under construction.
